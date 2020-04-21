1ad4ae7201d2c2d8e0b4b5d8cf20fe745ee97343
EC2 Default User <ec2-user@ip-172-31-30-59.us-east-2.compute.internal>
2020-04-21 10:24:57 +0000

    課題提出

---
+++ practice4.php
@@ -0,0 +1,9 @@
+<?php
+
+// $tech_boostに “tech “という文字列を代入
+$tech_boost = "tech ";
+
+// $tech_boostに “tech “という文字列を代入複合演算を使って、echo $tech_boostと実行すると「tech boost」と表示
+$tech_boost .= "boost";
+
+echo $tech_boost;
